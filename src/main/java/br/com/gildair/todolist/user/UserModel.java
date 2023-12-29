922caf68b1d8cee507bc5e0c9832893e6b6bdcf1
gildairmoreira <104792309+gildairmoreira@users.noreply.github.com>
2023-12-29 14:47:31 -0300

    refact imp

--- src/main/java/br/com/gildair/todolist/user/UserModel.java
+++ src/main/java/br/com/gildair/todolist/user/UserModel.java
@@ -9,7 +9,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.UniqueConstraint;
 import lombok.Data;
 
 @Data
